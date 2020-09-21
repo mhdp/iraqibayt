c519374ee96469b7c4828b02a94d00c126dbab09
mohammad <mhd.programmer@outlook.com>
2020-09-21 12:06:32 +0300

    Adding fonts to assets

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'ArabicKufi',
       ),
       // home: MyHomePage(title: 'البيت العراقي'),
       routes: {
